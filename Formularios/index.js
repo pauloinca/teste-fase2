ba9b58469448135a7f1ec11bbf9c3b7c84726beb
pauloinca <paulohincar@gmail.com>
2023-01-07 17:26:40 -0300

    Add formulario

--- Formularios/index.js
+++ Formularios/index.js
@@ -1,5 +1,17 @@
 const API = "https://api.npoint.io/cbf47d996930ba758a04";
 
+//Comparer Function
+function GetSortOrder(prop) {
+    return function (a, b) {
+        if (a[prop] > b[prop]) {
+            return 1;
+        } else if (a[prop] < b[prop]) {
+            return -1;
+        }
+        return 0;
+    };
+}
+
 $(document).ready(function () {
     // Example starter JavaScript for disabling form submissions if there are invalid fields
     (function () {
@@ -27,9 +39,24 @@ $(document).ready(function () {
 
     $(function populateBairroDropdown() {
         $.getJSON(API, function (data) {
+            data.sort(GetSortOrder("nome"));
             $.each(data, function (i, option) {
                 $("#bairro").append($("<option/>").attr("value", option.id).text(option.nome));
             });
         });
+    });
+});
+
+$(document).ready(function () {
+    $("#phone").keypress(function (e) {
+        if (e.keyCode != 8 && e.keyCode != 46) {
+            var text = this.value;
+            if (text.length == 0) {
+                this.value = text + "(";
+            }
+            if (text.length == 3) {
+                this.value = text + ")";
+            }
+        }
     });
 });
